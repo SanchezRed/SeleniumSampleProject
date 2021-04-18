728e9adbc0f0978fe3365f7fc45643cd5ab9e695
SanchezRed <82439397+SanchezRed@users.noreply.github.com>
2021-04-18 21:03:34 +0300

    1st test

--- src/test/java/Hillel/SimpleTest.java
+++ src/test/java/Hillel/SimpleTest.java
@@ -1,6 +1,20 @@
 package Hillel;
 
-public class SimpleTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class SimpleTest {
+    WebDriver driver;
+    @Test
+    public void checkMethod() {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\berez\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://research.mawashi.tools:8080/");
+        driver.findElement(By.cssSelector("input[name=\"username\"]")).sendKeys("dec21");
+        driver.findElement(By.cssSelector("input[name=\"username\"]")).sendKeys("asdffdsA1");
+        driver.findElement(By.cssSelector("input.btn")).click();
     }
+}
 
